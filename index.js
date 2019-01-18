d7e32baa9d99aeb1200798f84a85e90c99b3b950
Horia Alungulesei <horia.alungulesei@ro.ibm.com>
2019-01-19 00:01:45 +0200

    simplify payload

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ const iotClient = require("ibmiotf");
 const dhtSensor = require('node-dht-sensor');
 const config = require('./config');
 
-const interval = 5000; // milliseconds
-
-let deviceClient = new iotClient.IotfDevice(config);
+const interval = 10000; // milliseconds
+const deviceClient = new iotClient.IotfDevice(config);
 
 deviceClient.connect();
 
 deviceClient.on('connect', function () {
-    let event = 0;
     setInterval(function function_name () {
-        event++;
         dhtSensor.read(22, 4, function(err, temperature, humidity) {
             if (!err) {
-                const data = { eventNumber: event,
-                    temperature: temperature.toFixed(1),
+                const data = { temperature: temperature.toFixed(1),
                     humidity: humidity.toFixed(1)
                 };
                 deviceClient.publish('data', 'json', data, 2);
