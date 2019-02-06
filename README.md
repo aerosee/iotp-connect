61c52d5495bf2a7fc27e42694c0a0357c7fd49e9
Horia Alungulesei <horia.alungulesei@ro.ibm.com>
2019-02-07 01:06:09 +0200

    small updates

--- README.md
+++ README.md
@@ -1,20 +1,13 @@
 # @aerosee/iotp-connect
-Connects the DHT22 sensor to IBM's IoT Platform. To be used on a Raspberry Pi.
+Node.js application that connects the DHT22 temperature and humidity sensor to the IBM IoT platform, using Raspberry Pi.
 
 ## Prerequisites
 
-Run the `prereq.sh` script, it should prompt for the root password to install the required [BCM 2835](http://www.airspayce.com/mikem/bcm2835/) library.
-
-```
-chmod +x prereq.sh | ./prereq.sh
-npm install
-```
-
-## IBM IoT Configuration
-
-Create a new device type and a new device in the IBM IoT platform. Details here: https://console.bluemix.net/docs/services/IoT/iotplatform_task.html#iotplatform_subtask1.
-
-Configure fields in the `config.json` file with the respective org, device type, device ID and token from the IBM IoT platform. 
+ * Clone the repo and run `./prereq.sh`. It should prompt you for the root password to install the required [BCM 2835](http://www.airspayce.com/mikem/bcm2835/) library.
+ Alternatively, download and install it using the instructions from that page.
+ * Run `npm install`.
+ * Create a new *device type* and a new *device* in the IBM IoT platform. Details here: https://console.bluemix.net/docs/services/IoT/iotplatform_task.html#iotplatform_subtask1.
+ * Configure the `config.json` file with the *organization*, *device type*, *device ID* and *token* from the IBM IoT platform. Set other config fields as needed. 
 
 ## Usage
 
--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const iotClient = require("ibmiotf");
+const iotClient = require('ibmiotf');
 const dhtSensor = require('node-dht-sensor');
 const config = require('./config');
 
-const interval = 10000; // milliseconds
 const deviceClient = new iotClient.IotfDevice(config.iot);
 
 deviceClient.connect();
 
 deviceClient.on('connect', function () {
-    setInterval(function () {
-        dhtSensor.read(config.sensor, config.gpio, function(err, temperature, humidity) {
-            if (!err) {
-                const data = { temperature: temperature.toFixed(1),
-                    humidity: humidity.toFixed(1)
-                };
-                deviceClient.publish('data', 'json', data, config.qos);
-            } else {
-                console.error("Sensor read error: " + err);
-            }
-        });
-    }, interval);
+  setInterval(function () {
+    dhtSensor.read(config.sensor, config.gpio, function(err, temperature, humidity) {
+      if (!err) {
+        const data = {
+          temperature: temperature.toFixed(1),
+          humidity: humidity.toFixed(1)
+        };
+        deviceClient.publish('data', 'json', data, config.qos);
+      } else {
+        console.error('Sensor read error: ' + err);
+      }
+    });
+  }, config.interval);
 });
 
-deviceClient.on("error", function (err) {
-    console.error("IoT error: " + err);
+deviceClient.on('error', function (err) {
+  console.error('IoT error: ' + err);
 });
