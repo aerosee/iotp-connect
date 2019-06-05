bf87fdd52d37df562c4c6b140eff790f665d3fe0
Horia Alungulesei <horia.alungulesei@ro.ibm.com>
2019-06-06 00:11:32 +0300

    add logging

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const iotClient = require('ibmiotf');
 const dhtSensor = require('node-dht-sensor').promises;
 const config = require('./config');
+const { createLogger, format, transports } = require('winston');
 
 const deviceClient = new iotClient.IotfDevice(config.iot);
+
+const { combine, timestamp, prettyPrint } = format;
+const logger = createLogger({
+  format: combine(
+    timestamp(),
+    prettyPrint()
+  ),
+  transports: [
+    new transports.Console()
+  ]
+});
 
 deviceClient.connect();
 
@@ -15,13 +27,14 @@ deviceClient.on('connect', function () {
           humidity: res.humidity.toFixed(1)
         };
         deviceClient.publish('data', 'json', data, config.qos);
+        logger.info(data);
       },
       err => {
-        console.error('Sensor read error', err);
+        logger.error('Sensor read error', err);
       })
   }, config.interval);
 });
 
 deviceClient.on('error', function (err) {
-  console.error('IoT error', err);
+  logger.error('IoT error', err);
 });
